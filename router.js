c92435ae047d5a29fd51d4fa75e1792ff64e4c30
xizihang <xizihang@agree.com.cn>
2021-11-03 10:08:58 +0800

    Update router.js
    增加一级目录

--- router.js
+++ router.js
@@ -77,6 +77,30 @@
 		},
 
 		{
+			title: '编程语言-Java',
+			childList: [
+			
+			{
+				title: 'HTTP',
+				childList: [
+			
+				{
+					title: '1.1',
+					childList: [
+					{
+						title: '协议规范',
+						href: '/md/网络/HTTP/1_1/协议规范',
+					}
+					]
+				}
+			
+				]
+			}
+
+			]
+		},
+
+		{
 			title: '工具',
 			childList: [
 			
@@ -87,6 +111,233 @@
 					title: 'wireshark抓包分析',
 					href: '/md/工具/wireshark/wireshark抓包分析',
 				}
+				]
+			}
+
+			]
+		},
+
+		{
+			title: '分布式',
+			childList: [
+			
+			{
+				title: 'HTTP',
+				childList: [
+			
+				{
+					title: '1.1',
+					childList: [
+					{
+						title: '协议规范',
+						href: '/md/网络/HTTP/1_1/协议规范',
+					}
+					]
+				}
+			
+				]
+			}
+
+			]
+		},
+
+		{
+			title: '测试',
+			childList: [
+			
+			{
+				title: 'HTTP',
+				childList: [
+			
+				{
+					title: '1.1',
+					childList: [
+					{
+						title: '协议规范',
+						href: '/md/网络/HTTP/1_1/协议规范',
+					}
+					]
+				}
+			
+				]
+			}
+
+			]
+		},
+
+		{
+			title: '运维',
+			childList: [
+			
+			{
+				title: 'HTTP',
+				childList: [
+			
+				{
+					title: '1.1',
+					childList: [
+					{
+						title: '协议规范',
+						href: '/md/网络/HTTP/1_1/协议规范',
+					}
+					]
+				}
+			
+				]
+			}
+
+			]
+		},
+
+		{
+			title: '监控',
+			childList: [
+			
+			{
+				title: 'HTTP',
+				childList: [
+			
+				{
+					title: '1.1',
+					childList: [
+					{
+						title: '协议规范',
+						href: '/md/网络/HTTP/1_1/协议规范',
+					}
+					]
+				}
+			
+				]
+			}
+
+			]
+		},
+
+		{
+			title: '业务',
+			childList: [
+			
+			{
+				title: 'HTTP',
+				childList: [
+			
+				{
+					title: '1.1',
+					childList: [
+					{
+						title: '协议规范',
+						href: '/md/网络/HTTP/1_1/协议规范',
+					}
+					]
+				}
+			
+				]
+			}
+
+			]
+		},
+
+		{
+			title: '需求',
+			childList: [
+			
+			{
+				title: 'HTTP',
+				childList: [
+			
+				{
+					title: '1.1',
+					childList: [
+					{
+						title: '协议规范',
+						href: '/md/网络/HTTP/1_1/协议规范',
+					}
+					]
+				}
+			
+				]
+			}
+
+			]
+		},
+
+		{
+			title: '架构',
+			childList: [
+			
+			{
+				title: '综述',
+				href: '/md/架构/main'
+			},
+
+			{
+				title: '基础知识',
+				childList: [
+			
+				{
+					title: '如何设计系统的架构？',
+					href: '/md/架构/基础知识/如何设计系统的架构',
+				},
+			
+				{
+					title: '什么是架构方法？',
+					href: '/md/架构/基础知识/什么是架构风格',
+				},
+			
+				{
+					title: '什么是架构风格？',
+					href: '/md/架构/基础知识/什么是架构风格',
+				}
+
+				]
+			}
+
+			]
+		},
+
+
+		{
+			title: '产品',
+			childList: [
+			
+			{
+				title: '基础知识',
+				childList: [
+			
+				{
+					title: '1.1',
+					childList: [
+					{
+						title: '协议规范',
+						href: '/md/网络/HTTP/1_1/协议规范',
+					}
+					]
+				}
+			
+				]
+			}
+
+			]
+		},
+
+		{
+			title: '开源',
+			childList: [
+			
+			{
+				title: '基础知识',
+				childList: [
+			
+				{
+					title: '1.1',
+					childList: [
+					{
+						title: '协议规范',
+						href: '/md/网络/HTTP/1_1/协议规范',
+					}
+					]
+				}
+			
 				]
 			}
 
